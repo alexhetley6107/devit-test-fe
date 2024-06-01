eda586f7cba1a90cff8e40cd0ae576c56a80933d
alexhetley6107 <hetleyofficial@gmail.com>
2024-06-01 15:22:24 +0300

    feat: init project

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Container, CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Container>HHH</Container>
+    </>
+  );
+}
+
+export default App;
