b040a22e3b58e6e7a6c0f8cc1e59a952c757f079
alexhetley6107 <hetleyofficial@gmail.com>
2024-06-03 11:52:59 +0300

    feat: axios

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,40 @@
 import { Paper, Stack } from '@mui/material';
 import { Layout, Input, Button, Item } from '../shared/ui';
 import { ChangeEvent, useState } from 'react';
+import { axiosBase } from '../shared/axios';
+import { REQUEST_AMOUNT } from '../shared/constants';
 
 function App() {
   const [inputValue, setInputValue] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => setInputValue(+e.target.value);
+
+  const handleStart = async () => {
+    setIsLoading(true);
+    try {
+      const promises = [];
+
+      for (let i = 1; i <= REQUEST_AMOUNT; i++) {
+        const promise = axiosBase.get(`/api?itemRef=${i}`);
+        promises.push(promise);
+      }
+
+      const responses = await Promise.all(promises);
+
+      console.log('responses', responses);
+    } catch (error) {
+      console.log('error', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Layout>
       <Paper elevation={3}>
         <Stack width="300px" spacing={2} m={2}>
-          <Input value={inputValue} onChange={handleChange} />
-          <Button loading={false} disabled={false} />
+          <Input value={inputValue} onChange={handleChange} disabled={isLoading} />
+          <Button loading={isLoading} disabled={isLoading} onClick={handleStart} />
         </Stack>
       </Paper>
 
---
+++ src/shared/constants/index.ts
@@ -0,0 +1,1 @@
+export const REQUEST_AMOUNT = 1000;
